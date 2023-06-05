f47835ef71809727109921f9cd75254d021c85a4
thaiduongme <thaiduong.personal@gmail.com>
2023-06-05 15:59:59 +0700

    update entities config

--- src/database/datasource.ts
+++ src/database/datasource.ts
@@ -4,6 +4,8 @@ import { config } from '../config/configuration';
 import { AccountEntity } from '../modules/auth/entities/account.entity';
 import { RoleEntity } from '../modules/auth/entities/role.entity';
 import { TokenEntity } from '../modules/auth/entities/token.entity';
+import { WishlistEntity } from '../modules/user/entities/wishlist.entity';
+import { BugReportEntity } from '../modules/user/entities/bug-report.entity';
 
 export const BEDataSource = new DataSource({
   type: config.database.type,
@@ -14,7 +16,7 @@ export const BEDataSource = new DataSource({
   database: config.database.name,
   synchronize: true,
   logging: true,
-  entities: [AccountEntity, RoleEntity, TokenEntity],
+  entities: [AccountEntity, RoleEntity, TokenEntity, WishlistEntity, BugReportEntity],
   subscribers: [],
   migrations: [],
 });
