9f69f3c248362ac135760938ff7f4f1abc528872
thaiduongme <thaiduong.personal@gmail.com>
2023-06-20 14:54:12 +0700

    add gateway init

---
+++ gateway/src/config/configuration.ts
@@ -0,0 +1,37 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.gateway' });
+
+const REQUIRED_ENV_VARS = ['GATEWAY_LISTEN_PORT', 'AUTH_SERVICE_HOST', 'AUTH_SERVICE_PORT', 'AUTH_SERVICE_PATH'];
+
+interface Configuration {
+  gateway: {
+    listenPort: number;
+  };
+  authService: {
+    host: string;
+    port: number;
+    path: string;
+  };
+}
+
+export const config: Configuration = {
+  gateway: {
+    listenPort: parseInt(process.env.GATEWAY_LISTEN_PORT as string, 10) ?? 3000,
+  },
+  authService: {
+    host: (process.env.AUTH_SERVICE_HOST as string) ?? 'localhost',
+    port: parseInt(process.env.AUTH_SERVICE_PORT as string, 10) ?? 3001,
+    path: (process.env.AUTH_SERVICE_PATH as string) ?? 'auth',
+  },
+};
+
+export const validateEnvironmentVars = (): void => {
+  const missingRequiredEnvVars: string[] = [];
+  REQUIRED_ENV_VARS.forEach((envVar) => {
+    if (!(envVar in process.env)) missingRequiredEnvVars.push(envVar);
+  });
+  if (missingRequiredEnvVars.length !== 0) {
+    throw new Error(`Missing required environment variables: [${missingRequiredEnvVars.join(', ')}]`);
+  }
+};
