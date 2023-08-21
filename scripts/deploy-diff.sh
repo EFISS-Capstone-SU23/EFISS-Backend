673a7b56b6d6d1176e4fc2aa0397174fc37122b8
thaiminhpv <thaiminhpv@gmail.com>
2023-08-21 13:37:25 +0700

    change Docker registry repository to efiss-duong

--- scripts/deploy-diff.sh
+++ scripts/deploy-diff.sh
@@ -40,7 +40,7 @@ done <<< "$changed_ts_files"
 
 echo "Selected microservices to deploy: ${selected[@]}"
 
-url="asia-southeast1-docker.pkg.dev/efiss-394203/efiss"
+url="asia-southeast1-docker.pkg.dev/efiss-duong/efiss"
 timestamp=$(date +%Y%m%d%H%M%S)
 
 # Build and push the selected microservices
